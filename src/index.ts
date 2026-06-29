d3d38860b7946cc52e0ea48dc163416a16ac836e
Marc Radmacher <marc.radmacher@hetkamp-gmbh.de>
2026-06-29 11:07:42 +0200

    fix: linting

--- src/index.ts
+++ src/index.ts
@@ -1,176 +1,165 @@
-import * as core from "@actions/core";
-import * as exec from "@actions/exec";
-import extract from "extract-zip";
-import * as fs from "fs";
-import { XMLParser } from "fast-xml-parser";
-import XMLBuilder from "fast-xml-builder"
-import path from "path";
-import { ZipArchive } from "archiver";
+import * as fs from 'node:fs';
+import path from 'node:path';
+import * as core from '@actions/core';
+import * as exec from '@actions/exec';
+import { ZipArchive } from 'archiver';
+import extract from 'extract-zip';
+import XMLBuilder from 'fast-xml-builder';
+import { XMLParser } from 'fast-xml-parser';
 
 async function run() {
-  const version = core.getInput("version").trim();
-  const repository = core.getInput("repository");
-  const pat = core.getInput("token");
-  const pushToReg = core.getBooleanInput("push");
-  const skipDuplicate = core.getBooleanInput("skipduplicate");
-  const dir = core.getInput("directory");
-  const base = process.env.GITHUB_WORKSPACE as string;
-  const baseDirectory = path.join(base, dir);
+	const version = core.getInput('version').trim();
+	const repository = core.getInput('repository');
+	const pat = core.getInput('token');
+	const pushToReg = core.getBooleanInput('push');
+	const skipDuplicate = core.getBooleanInput('skipduplicate');
+	const dir = core.getInput('directory');
+	const base = process.env.GITHUB_WORKSPACE as string;
+	const baseDirectory = path.join(base, dir);
 
-  core.debug("Repository: " + repository);
-  core.debug("Base directory: " + baseDirectory);
-  core.debug("Version: " + version);
+	core.debug(`Repository: ${repository}`);
+	core.debug(`Base directory: ${baseDirectory}`);
+	core.debug(`Version: ${version}`);
 
-  // Validate Version
-  if (version) {
-    const versionRegex = new RegExp("^([0-9]{1,}).([0-9]{1,}).([0-9]{1,})(.([0-9]{1,}))?$");
-    if (!versionRegex.exec(version)) {
-      core.debug(`The version string *${version}* does not match ^([0-9]{1,}).([0-9]{1,}).([0-9]{1,})(.([0-9]{1,}))?$`)
-      throw new Error(
-        "Version *" + version + "* is not a valid version. (x.x.x.x or x.x.x)"
-      );
-    }
-  }
+	// Validate Version
+	if (version) {
+		const versionRegex = /^([0-9]{1,}).([0-9]{1,}).([0-9]{1,})(.([0-9]{1,}))?$/;
+		if (!versionRegex.exec(version)) {
+			core.debug(`The version string *${version}* does not match ^([0-9]{1,}).([0-9]{1,}).([0-9]{1,})(.([0-9]{1,}))?$`);
+			throw new Error(`Version *${version}* is not a valid version. (x.x.x.x or x.x.x)`);
+		}
+	}
 
-  // list all files & find .nupkg
-  let nupkgFile;
-  const nupkgFiles = fs.readdirSync(baseDirectory);
-  console.log("Searching for .nupkg...");
-  core.debug("Found these files: " + nupkgFiles.join("; "));
-  for (const file of nupkgFiles) {
-    if (file.endsWith(".nupkg")) {
-      nupkgFile = file;
-      break;
-    }
-  }
-  if (!nupkgFile) {
-    throw new Error("Could not find .nupkg file. Make sure it got generated.");
-  }
-  console.log("Found .nupkg file. Extracting...");
+	// list all files & find .nupkg
+	let nupkgFile: string | undefined;
+	const nupkgFiles = fs.readdirSync(baseDirectory);
+	console.log('Searching for .nupkg...');
+	core.debug(`Found these files: ${nupkgFiles.join('; ')}`);
+	for (const file of nupkgFiles) {
+		if (file.endsWith('.nupkg')) {
+			nupkgFile = file;
+			break;
+		}
+	}
+	if (!nupkgFile) {
+		throw new Error('Could not find .nupkg file. Make sure it got generated.');
+	}
+	console.log('Found .nupkg file. Extracting...');
 
-  await extract(path.join(baseDirectory, nupkgFile), {
-    dir: path.join(baseDirectory, "extracted-nupkg"),
-  });
+	await extract(path.join(baseDirectory, nupkgFile), {
+		dir: path.join(baseDirectory, 'extracted-nupkg'),
+	});
 
-  // Find .nuspec file
-  console.log("Extracted .nupkg file content. Searching for .nuspec file...");
-  let nuspecFile;
-  const nuspecFiles = fs.readdirSync(
-    path.join(baseDirectory, "extracted-nupkg")
-  );
-  for (const file of nuspecFiles) {
-    if (file.endsWith(".nuspec")) {
-      nuspecFile = file;
-      break;
-    }
-  }
-  if (!nuspecFile) {
-    throw new Error("Could not find .nuspec file.");
-  }
-  console.log("Found .nuspec file. Reading File...");
+	// Find .nuspec file
+	console.log('Extracted .nupkg file content. Searching for .nuspec file...');
+	let nuspecFile: string | undefined;
+	const nuspecFiles = fs.readdirSync(path.join(baseDirectory, 'extracted-nupkg'));
+	for (const file of nuspecFiles) {
+		if (file.endsWith('.nuspec')) {
+			nuspecFile = file;
+			break;
+		}
+	}
+	if (!nuspecFile) {
+		throw new Error('Could not find .nuspec file.');
+	}
+	console.log('Found .nuspec file. Reading File...');
 
-  // Read .nuspec file
-  const nuspecFileContent = fs.readFileSync(
-    path.join(baseDirectory, "extracted-nupkg", nuspecFile)
-  );
-  core.debug(".nuspec content: " + nuspecFileContent);
-  console.log("Read .nuspec file. Parsing file...");
+	// Read .nuspec file
+	const nuspecFileContent = fs.readFileSync(path.join(baseDirectory, 'extracted-nupkg', nuspecFile));
+	core.debug(`.nuspec content: ${nuspecFileContent}`);
+	console.log('Read .nuspec file. Parsing file...');
 
-  // Parse .nuspec File
-  const parser = new XMLParser({ ignoreAttributes: false });
-  const jsonObject = parser.parse(nuspecFileContent);
-  core.debug("Parsed .nuspec content: " + JSON.stringify(jsonObject));
-  console.log("Parsed .nuspec file. Modifying file...");
+	// Parse .nuspec File
+	const parser = new XMLParser({ ignoreAttributes: false });
+	const jsonObject = parser.parse(nuspecFileContent);
+	core.debug(`Parsed .nuspec content: ${JSON.stringify(jsonObject)}`);
+	console.log('Parsed .nuspec file. Modifying file...');
 
-  // Add fields to object
-  jsonObject.package.metadata.repository = {
-    "@_type": "git",
-    "@_url": `https://github.com/${repository}`,
-  };
+	// Add fields to object
+	jsonObject.package.metadata.repository = {
+		'@_type': 'git',
+		'@_url': `https://github.com/${repository}`,
+	};
 
-  // Change Version
-  if (version) {
-    jsonObject.package.metadata.version = version;
-    console.log("Set version to " + version);
-  }
+	// Change Version
+	if (version) {
+		jsonObject.package.metadata.version = version;
+		console.log(`Set version to ${version}`);
+	}
 
-  // Write new .nuspec File
-  const builder = new XMLBuilder({ format: true, ignoreAttributes: false });
-  const xmlContent = builder.build(jsonObject);
-  core.debug("Modified XML: " + xmlContent);
-  fs.writeFileSync(
-    path.join(baseDirectory, "extracted-nupkg", nuspecFile),
-    xmlContent
-  );
-  console.log("Modified .nuspec file. Deleting old .nupkg...");
+	// Write new .nuspec File
+	const builder = new XMLBuilder({ format: true, ignoreAttributes: false });
+	const xmlContent = builder.build(jsonObject);
+	core.debug(`Modified XML: ${xmlContent}`);
+	fs.writeFileSync(path.join(baseDirectory, 'extracted-nupkg', nuspecFile), xmlContent);
+	console.log('Modified .nuspec file. Deleting old .nupkg...');
 
-  // delete old .nupkg
-  fs.rmSync(path.join(baseDirectory, nupkgFile));
-  console.log("Deleted old .nupkg. Writing new .nupkg...");
+	// delete old .nupkg
+	fs.rmSync(path.join(baseDirectory, nupkgFile));
+	console.log('Deleted old .nupkg. Writing new .nupkg...');
 
-  // Zip files
-  const arch = new ZipArchive();
-  let outputFile = nupkgFile;
-  if (version) {
-    const nugetName = nupkgFile.split(".")[0];
-    if (!nugetName) {
-      throw new Error("Could not parse NuGet file name");
-    }
-    outputFile = `${nugetName}.${version}.nupkg`;
-    core.debug(`New output file name: ${outputFile}`);
-  }
-  const output = fs.createWriteStream(path.join(baseDirectory, outputFile));
-  output.on("close", () => {
-    console.log(
-      `Wrote new .nupkg (${arch.pointer()} bytes). ${
-        pushToReg ? "Pushing .nupkg to GitHub registry..." : ""
-      }`
-    );
-  });
-  arch.on("error", (err) => {
-    console.error("Encountered an error while zipping files.");
-    throw err;
-  });
-  arch.on("warning", (err) => {
-    console.warn("Encountered an warning while zipping files: ", err);
-  });
-  arch.pipe(output);
-  arch.directory(path.join(baseDirectory, "extracted-nupkg"), false);
-  await arch.finalize();
+	// Zip files
+	const arch = new ZipArchive();
+	let outputFile = nupkgFile;
+	if (version) {
+		const nugetName = nupkgFile.split('.')[0];
+		if (!nugetName) {
+			throw new Error('Could not parse NuGet file name');
+		}
+		outputFile = `${nugetName}.${version}.nupkg`;
+		core.debug(`New output file name: ${outputFile}`);
+	}
+	const output = fs.createWriteStream(path.join(baseDirectory, outputFile));
+	output.on('close', () => {
+		console.log(
+			`Wrote new .nupkg (${arch.pointer()} bytes). ${pushToReg ? 'Pushing .nupkg to GitHub registry...' : ''}`,
+		);
+	});
+	arch.on('error', (err) => {
+		console.error('Encountered an error while zipping files.');
+		throw err;
+	});
+	arch.on('warning', (err) => {
+		console.warn('Encountered an warning while zipping files: ', err);
+	});
+	arch.pipe(output);
+	arch.directory(path.join(baseDirectory, 'extracted-nupkg'), false);
+	await arch.finalize();
 
-  if (!pushToReg) return;
+	if (!pushToReg) return;
 
-  // Push .nupkg to GitHub Registry
-  const owner = repository.split("/")[0];
-  if (!owner) {
-    throw new Error(
-      "Could not find owner of repository. Make sure the repository you passed is valid (<Owner>/<Repository>)"
-    );
-  }
-  exec.exec(
-    "nuget",
-    [
-      "push",
-      path.join(baseDirectory, outputFile),
-      "-Source",
-      `https://nuget.pkg.github.com/${owner}/index.json`,
-      "-ApiKey",
-      pat,
-      "-NonInteractive",
-      `${skipDuplicate ? "-SkipDuplicate" : ""}`,
-    ],
-    {
-      listeners: {
-        stdout: (data: Buffer) => {
-          console.log(data.toString());
-        },
-        stderr: (data: Buffer) => {
-          console.error(data.toString());
-        },
-      },
-    }
-  );
-  console.log("Pushed .nupkg to GitHub repository.");
+	// Push .nupkg to GitHub Registry
+	const owner = repository.split('/')[0];
+	if (!owner) {
+		throw new Error(
+			'Could not find owner of repository. Make sure the repository you passed is valid (<Owner>/<Repository>)',
+		);
+	}
+	exec.exec(
+		'nuget',
+		[
+			'push',
+			path.join(baseDirectory, outputFile),
+			'-Source',
+			`https://nuget.pkg.github.com/${owner}/index.json`,
+			'-ApiKey',
+			pat,
+			'-NonInteractive',
+			`${skipDuplicate ? '-SkipDuplicate' : ''}`,
+		],
+		{
+			listeners: {
+				stdout: (data: Buffer) => {
+					console.log(data.toString());
+				},
+				stderr: (data: Buffer) => {
+					console.error(data.toString());
+				},
+			},
+		},
+	);
+	console.log('Pushed .nupkg to GitHub repository.');
 }
 
 run();
